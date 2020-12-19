5298b8dd0ba6247842631e7c974b4725f641f782
darkzwer <darkzwer@yandex.ru>
2020-12-20 00:07:24 +0200

    Initial Commit

---
+++ UI_Slots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI_Slots
+//
+//  Created by Админ on 20.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI_Slots/UI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_SlotsApp.swift
+//  UI_Slots
+//
+//  Created by Админ on 20.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct UI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
